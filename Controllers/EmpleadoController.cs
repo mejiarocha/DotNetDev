39ea8f48119b10dcd5c54e1193f79f90a5da0b1d
Juan Mejia <mejiarocha.ja@gmail.com>
2024-09-18 21:22:52 -0500

    First Commit

---
+++ Controllers/EmpleadoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WebAPICrudTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPICrudTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+
+        private readonly DbcrudContext dbContext;
+
+        public EmpleadoController(DbcrudContext _dbcrudContext)
+        {
+            dbContext = _dbcrudContext;
+        }
+
+        [HttpGet]
+        [Route("lista")]
+        public async Task<IActionResult> Get()
+        {
+            var listaEmpleado = await dbContext.Empleados.ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, listaEmpleado);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create(Empleado E)
+        {
+            try
+            {
+                // Código que podría lanzar una excepción
+                await dbContext.Empleados.AddAsync(E);
+            }
+            catch (Exception ex)
+            {
+                // Manejo de la excepción
+                Console.WriteLine(ex.Message);  // Muestra el mensaje de error
+            }
+        }
+    }
+}
